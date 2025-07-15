3e40fd2a54bb60aee6a07563c9d74a9abcae19c3
Michal92299 <michal.99967@gmail.com>
2025-07-15 13:48:25 +0200

    Create utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,12 @@
+use colored::*;
+
+pub fn print_banner() {
+    println!("{}", r#"
+        ╔════════════════════════════╗
+        ║   Zenit Package Manager    ║
+        ╚════════════════════════════╝
+    "#.bold().cyan());
+    println!("{}", "Witaj w Zenit!".bold().green());
+    println!("{}", "Minimalistyczny i szybki menedżer pakietów dla Arch Linux.".italic());
+    println!();
+}
