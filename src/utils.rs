4ea8d8c6001687d1a594de21e7a77c01525b9e63
Michal92299 <michal.99967@gmail.com>
2025-07-15 14:08:46 +0200

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -2,9 +2,9 @@ use colored::*;
 
 pub fn print_banner() {
     println!("{}", r#"
-        ╔════════════════════════════╗
-        ║   Zenit Package Manager    ║
-        ╚════════════════════════════╝
+    ╔════════════════════════════╗
+    ║   Zenit Package Manager    ║
+    ╚════════════════════════════╝
     "#.bold().cyan());
     println!("{}", "Witaj w Zenit!".bold().green());
     println!("{}", "Minimalistyczny i szybki menedżer pakietów dla Arch Linux.".italic());
